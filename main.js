d963bbfc997f9bfb5a5fa889ebe74b058d705dba
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-11-08 22:42:54 +0100

    completato il sito - ho utilizzato sia il cambio di src per mostrare un'immagine differente, che il metodo con hidden (ho usato la proprietà visually-hidden di BS)

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+// lampadina
 let lightOn = "img/lightbulb_on.png";
 let lightOff = "img/lightbulb_off.png";
 let lightbulb = document.getElementById("lightbulb");
 let lightSwitch = document.getElementById("lightSwitch");
+
 
 function turnLightOnOrOff() {
     if (lightbulb.getAttribute("src") == lightOn) {
@@ -14,3 +16,12 @@ function turnLightOnOrOff() {
 }
 
 lightSwitch.addEventListener("click", turnLightOnOrOff);
+
+// transistor
+let transistorButton = document.getElementById("transistorSwitch");
+let transistorOnClassList = document.getElementById("transistorOn").classList;
+let transistorOffClassList = document.getElementById("transistorOff").classList;
+transistorButton.addEventListener("click", () => {
+    transistorOnClassList.toggle("visually-hidden"); 
+    transistorOffClassList.toggle("visually-hidden");
+    })
